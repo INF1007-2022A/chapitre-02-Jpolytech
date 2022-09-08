14ba5261557cc0722dbce730e31bd744817ad2f5
Jpolytech <113134412+Jpolytech@users.noreply.github.com>
2022-09-08 14:19:24 -0400

    Update exercice.py

--- exercice.py
+++ exercice.py
@@ -2,6 +2,9 @@
 # -*- coding: utf-8 -*-
 def majuscule(mot):
     # TODO completer la fonction ici
+    x = input("entrer une lettre en minuscule :")
+    print("Le code ASCII de ",x," est : ",ord(x))
+    x = ord(x) - 32
     return mot
 
 
